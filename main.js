7d97d506077ecd0db781ae0584b958a84dffdd08
quockhanhd <dqk0309@gmail.com>
2018-11-05 15:43:26 +0700

    play stream local

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log($);
+function openStream() {
+    const config = {audio:true, video:true};
+ return navigator.mediaDevices.getUserMedia(config);   
+}
+function playStream(idVideoTag, stream) {
+    const video = $(document.getElementById(idVideoTag));
+    video.srcObject = stream;
+    video.play();
+}
+openStream()
+.then(stream => playStream('localStream',stream));
